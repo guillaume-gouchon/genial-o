9ff3fc3a65e2025dc68b8d58d3260b7d0021911c
Guillaume Gouchon <guillaume.gouchon@gmail.com>
2017-08-07 17:43:25 +0100

    u

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-print
-
 @app.route("/")
 def hello():
     return "Hello World!"
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python robot.py &
 export FLASK_APP=server.py;
-python -m flask run
+python -m flask run --host=0.0.0.0
