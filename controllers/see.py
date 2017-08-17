dbef6babe615e04072da51493f00494b61114d74
Guillaume Gouchon <guillaume.gouchon@gmail.com>
2017-08-17 19:27:53 +0100

    u

--- controllers/see.py
+++ controllers/see.py
@@ -10,22 +10,22 @@ def take_picture():
 
     if 'pi_camera' in globals():
         frame = pi_camera.get_frame()
-        print(frame)
-
-
-    with picamera.PiCamera() as camera:
-        camera.rotation = 180
-        camera.resolution = (1200, 800)
-        print('camera is ready, warming up...')
-        time.sleep(CAMERA_WARM_UP)
-        camera.capture(LATEST_PIC_PATH)
-        print('picture taken')
+        file = open(LATEST_PIC_PATH, 'w')
+        file.write(frame)
+        file.close()
+    else:
+        with picamera.PiCamera() as camera:
+            camera.rotation = 180
+            camera.resolution = (1200, 800)
+            print('camera is ready, warming up...')
+            time.sleep(CAMERA_WARM_UP)
+            camera.capture(LATEST_PIC_PATH)
+            print('picture taken')
 
 def generate_frames():
     global pi_camera
     pi_camera = Camera()
     while True:
         frame = pi_camera.get_frame()
-        print(frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
