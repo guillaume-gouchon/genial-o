d24af8892acdd067b5fc61e62e2dd06e0a1500a5
Guillaume Gouchon <guillaume.gouchon@gmail.com>
2017-08-17 16:51:45 +0100

    u

--- controllers/recognize.py
+++ controllers/recognize.py
@@ -4,14 +4,19 @@ import subprocess
 
 def guess():
     see.take_picture()
-    print("guessing...")
 
+    print("guessing...")
     command = 'python ../libs/classify_image.py --num_top_predictions 1 --image_file ' + see.LATEST_PIC_PATH
     result = subprocess.check_output(command.split(), stderr=subprocess.PIPE).decode('UTF-8')
-    guess = result.split(',')[0]
-    probability = float(re.findall(r"[-+]?\d*\.\d+|\d+", result)[0])
-    print("result =", guess, round(probability), '%')
+    split = result.split(',')
+    if len(split) > 0:
+        guess = split[0]
+        probability = float(re.findall(r"[-+]?\d*\.\d+|\d+", result)[0])
+        print("result =", guess, round(probability), '%')
 
-    output = "I guess it's a " + guess + " (" + str(round(probability)) + "%)"
+        output = "I guess it's a " + guess + " (" + str(round(probability)) + "%)"
+    else:
+        output = "I don't know what it is"
+
     display.print_text(output, 1)
     return output
