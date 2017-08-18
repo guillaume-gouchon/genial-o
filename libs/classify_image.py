a082d4a9c8dd6c99cd7da962bb442c4dd897a619
Guillaume Gouchon <guillaume.gouchon@gmail.com>
2017-08-18 14:22:38 +0100

    u

--- libs/classify_image.py
+++ libs/classify_image.py
@@ -174,15 +174,6 @@ def maybe_download_and_extract():
     os.makedirs(dest_directory)
   filename = DATA_URL.split('/')[-1]
   filepath = os.path.join(dest_directory, filename)
-  if not os.path.exists(filepath):
-    def _progress(count, block_size, total_size):
-      sys.stdout.write('\r>> Downloading %s %.1f%%' % (
-          filename, float(count * block_size) / float(total_size) * 100.0))
-      sys.stdout.flush()
-    filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
-    print()
-    statinfo = os.stat(filepath)
-    print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
   tarfile.open(filepath, 'r:gz').extractall(dest_directory)
 
 
@@ -204,7 +195,7 @@ if __name__ == '__main__':
   parser.add_argument(
       '--model_dir',
       type=str,
-      default='/app/imagenet',
+      default='/data',
       help="""\
       Path to classify_image_graph_def.pb,
       imagenet_synset_to_human_label_map.txt, and
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+pip install libs/tensorflow-1.1.0-cp27-none-linux_armv7l.whl && pip uninstall -y mock && pip install mock
 modprobe v4l2_common && python robot.py
