3649520fd720a6985db0d34841a8677df2bb5527
Guillaume Gouchon <guillaume.gouchon@gmail.com>
2017-08-22 12:19:35 +0100

    u

--- controllers/recognize.py
+++ controllers/recognize.py
@@ -1,7 +1,21 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import re
+import os.path
+import numpy as np
+import tensorflow as tf
+
 import see as see
 import display as display
-import subprocess
+
+
+model_dir = "/app/libs"
+image_path = see.LATEST_PIC_PATH
+
+# Creates graph from saved GraphDef.
+create_graph()
 
 def guess():
     see.take_picture()
@@ -9,18 +23,109 @@ def guess():
     print("guessing...")
     display.clear_text()
     display.print_text("Guessing...", 1)
-    result = subprocess.check_output(["python", "/app/libs/classify_image.py", "--num_top_predictions=1", "--image_file=" + see.LATEST_PIC_PATH], stderr=subprocess.PIPE).decode('UTF-8')
-    print(result)
-    split = result.split(',')
-    if len(split) > 0:
-        print("result =", split)
-        guess = split[0]
-        probability = float(re.findall(r"[-+]?\d*\.\d+|\d+", split[len(split) - 1])[0])
-
-        output = "Guess it's a " + guess + " " + str(round(probability)) + "%"
-    else:
-        output = "I don't know what it is"
-
+    result = run_inference_on_image();
+    output = "Guess it's a " + result[0] + " " + str(round(result[1])) + "%"
     display.clear_text()
     display.print_text(output, 1)
     return output
+
+def create_graph():
+  # Creates graph from saved graph_def.pb.
+  with tf.gfile.FastGFile(os.path.join(model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+    print("Graph created")
+
+def run_inference_on_image():
+  if not tf.gfile.Exists(image_path):
+    tf.logging.fatal('File does not exist %s', image_path)
+  image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+  with tf.Session() as sess:
+    # Some useful tensors:
+    # 'softmax:0': A tensor containing the normalized prediction across
+    #   1000 labels.
+    # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
+    #   float description of the image.
+    # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
+    #   encoding of the image.
+    # Runs the softmax tensor by feeding the image_data as input to the graph.
+    print("Session created")
+    softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+    predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+    predictions = np.squeeze(predictions)
+    print("Got predictions")
+
+    # Creates node ID --> English string lookup.
+    node_lookup = NodeLookup()
+
+    number_of_predictions = 1
+    top_k = predictions.argsort()[-number_of_predictions:][::-1]
+    for node_id in top_k:
+      human_string = node_lookup.id_to_string(node_id)
+      score = predictions[node_id]
+      print("prediction = ", human_string, score, "%")
+      return [human_string, score]
+
+
+class NodeLookup(object):
+  """Converts integer node ID's to human readable labels."""
+
+  def __init__(self, label_lookup_path=None, uid_lookup_path=None):
+    if not label_lookup_path:
+      label_lookup_path = os.path.join(model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
+    if not uid_lookup_path:
+      uid_lookup_path = os.path.join(model_dir, 'imagenet_synset_to_human_label_map.txt')
+    self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
+
+
+  def load(self, label_lookup_path, uid_lookup_path):
+    """Loads a human readable English name for each softmax node.
+
+    Args:
+      label_lookup_path: string UID to integer node ID.
+      uid_lookup_path: string UID to human-readable string.
+
+    Returns:
+      dict from integer node ID to human-readable string.
+    """
+    if not tf.gfile.Exists(uid_lookup_path):
+      tf.logging.fatal('File does not exist %s', uid_lookup_path)
+    if not tf.gfile.Exists(label_lookup_path):
+      tf.logging.fatal('File does not exist %s', label_lookup_path)
+
+    # Loads mapping from string UID to human-readable string
+    proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
+    uid_to_human = {}
+    p = re.compile(r'[n\d]*[ \S,]*')
+    for line in proto_as_ascii_lines:
+      parsed_items = p.findall(line)
+      uid = parsed_items[0]
+      human_string = parsed_items[2]
+      uid_to_human[uid] = human_string
+
+    # Loads mapping from string UID to integer node ID.
+    node_id_to_uid = {}
+    proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
+    for line in proto_as_ascii:
+      if line.startswith('  target_class:'):
+        target_class = int(line.split(': ')[1])
+      if line.startswith('  target_class_string:'):
+        target_class_string = line.split(': ')[1]
+        node_id_to_uid[target_class] = target_class_string[1:-2]
+
+    # Loads the final mapping of integer node ID to human-readable string
+    node_id_to_name = {}
+    for key, val in node_id_to_uid.items():
+      if val not in uid_to_human:
+        tf.logging.fatal('Failed to locate: %s', val)
+      name = uid_to_human[val]
+      node_id_to_name[key] = name
+
+    return node_id_to_name
+
+  def id_to_string(self, node_id):
+    if node_id not in self.node_lookup:
+      return ''
+    return self.node_lookup[node_id]
